a8d428d400feeb3550487672a2e556726caf5b06
Fayoke Adeyina <fayoaright@gmail.com>
2018-03-16 17:59:36 +0100

    first commit

---
+++ question.js
@@ -0,0 +1,38 @@
+var questions = [{
+	"question": "what is Computer science?",
+	"option1": "the study of problems, problem-solving, and the solutions that come out of the problem-solving process",
+	"option2": "the study of computers, hardware and software.",
+	"option3": "the study of computers, hardware and software.",
+	"option4": "the study of problems, problem-solving, and the solutions that come out of the problem-solving process",
+	"answer": "2"
+}, {
+	"question": "A computer scienctists' goal is __",
+	"option1": "to develop algorithm",
+	"option2": "to get good pay",
+	"option3": "to build the globe",
+	"option4": "to develop algorithm",
+	"answer": "1"
+}, {
+	"question": "What is a an algorithm?",
+	"option1": "a step by step way of making money",
+	"option2": "a step by step way of creating programmes",
+	"option3": "a step-by-step list of instructions for solving any instance of the problem that might arise",
+	"option4": "an easy step to computing",
+	"answer": "3"
+}, {
+	"question": "which one of these is not a programming language?",
+	"option1": "java",
+	"option2": "javascript",
+	"option3": "elexer",
+	"option4": "python",
+	"answer": "3"
+
+}, {
+	"question": "which one of these is correct?",
+	"option1": "html is used for styling webpage",
+	"option2": "javascript is used for styling webpage",
+	"option3": "css is used for styling webpage",
+	"option4": "php is used for styling webpage",
+	"answer": "3"
+}
+]
